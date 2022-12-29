b7b912e6485f30d419ecccfa2f625127b32bb83a
Rajat Mishra <mishrarajat21849@gmail.com>
2022-12-29 15:10:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './Login';
+
+import { useState } from 'react';
+import Home from './Home';
 
 function App() {
+  const [islogin,setislogin]=useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        islogin? <Login setlogin={setislogin}/>:<Home/>
+      }
+
+    {!islogin?<button className='logout' onClick={()=>setislogin(true)}>logout</button>:null}
     </div>
   );
 }
